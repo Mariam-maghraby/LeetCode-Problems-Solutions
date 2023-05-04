137ddc1a8ae26dc1ad1cbe6db4ee45295ac5446e
Mariam Maghraby <61349498+Mariam-maghraby@users.noreply.github.com>
2023-05-04 17:31:41 +0300

    Time: 199 ms (14.60%), Space: 70.9 MB (28.72%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -2,11 +2,14 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         bool flag = false;
-       sort(nums.begin(), nums.end());
-        for (int i=0; i<(nums.size()-1); i++){
-            if(nums[i] == nums[i+1]){
+        map<int, int> map;
+        for (int i=0; i<nums.size(); i++){
+            if (map.count(nums[i]) > 0){
                 flag = true;
                 break;
+            }
+            else{
+                map.insert({nums[i], i});
             }
         }
         return flag; 
