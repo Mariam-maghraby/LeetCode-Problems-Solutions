9ef70dcc3c7acc9320314d80c30d8fe5f3558179
Mariam Maghraby <61349498+Mariam-maghraby@users.noreply.github.com>
2023-08-27 01:01:12 +0300

    Time: 0 ms (100.00%), Space: 9.1 MB (67.17%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        string common = "";
-        char letter ;
-        if(strs.size() == 1){
-            return strs[0];
-        }
+            string common = "";
+        
+        
         for (int i=0; i<strs[0].size(); i++){
+            char letter = strs[0][i] ;
             for(int j=1; j<strs.size(); j++){
-                if(strs[0][i] == strs[j][i]){
-                        letter = strs[0][i];
-                }
-                else{
+                if(strs[0][i] != strs[j][i]){
                   
                     return common;
                 }
@@ -21,5 +17,6 @@ public:
 
         }
         return common;
+        
     }
 };
