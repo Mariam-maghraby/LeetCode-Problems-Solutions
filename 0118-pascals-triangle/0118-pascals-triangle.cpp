46e44336f514c4936675211aac71e55f85356c6c
Mariam Maghraby <61349498+Mariam-maghraby@users.noreply.github.com>
2023-08-28 15:54:43 +0300

    Time: 3 ms (40.92%), Space: 6.7 MB (44.21%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-       vector<vector<int>> outerSet(numRows);
-       outerSet = {{1}};
+       vector<vector<int>> outerSet;
 
-       for(int i=1; i<numRows; i++){
-           vector<int> innerSet(i+1);
-           innerSet[0] = 1;
-           innerSet[i] = 1;
+       for(int i=0; i<numRows; i++){
+           vector<int> innerSet(i+1, 1);
            outerSet.push_back(innerSet);
            for(int j=1; j<i; j++){
                outerSet[i][j] = outerSet[i-1][j-1]+ outerSet[i-1][j];
