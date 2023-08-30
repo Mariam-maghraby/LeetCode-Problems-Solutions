14aa68064f2c34972870280cd6b83d7511b4baca
Mariam Maghraby <61349498+Mariam-maghraby@users.noreply.github.com>
2023-08-30 16:59:32 +0300

    Time: 6 ms (66.11%), Space: 7.2 MB (19.27%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -5,19 +5,16 @@ public:
             map<char, char> tMap;
             int i=0;
             while(i<s.length()){
-                if (sMap.count(s[i]) > 0 || tMap.count(t[i]) > 0){
-                    if(sMap[s[i]] != t[i] || tMap[t[i]] != s[i] ){
-                        return false;
-                    }
-                    else {
-                        i++;
-                    }
-                }
-                else {
-                    sMap[s[i]] = t[i];
-                    tMap[t[i]] = s[i];
-                    i++;
-                }
+               
+            if( (sMap[s[i]] && sMap[s[i]] != t[i]) || (tMap[t[i]] && tMap[t[i]] != s[i]) ){
+                return false;
+            }
+         
+            else {
+                sMap[s[i]] = t[i];
+                tMap[t[i]] = s[i];
+                i++;
+            }
             }
 
         return true;
