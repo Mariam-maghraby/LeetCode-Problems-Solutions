0c716927fd84adf0600ee5b48725d556510253f0
Mariam Maghraby <61349498+Mariam-maghraby@users.noreply.github.com>
2023-09-04 16:02:02 +0300

    Time: 12 ms (73.07%), Space: 20.4 MB (8.52%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -6,16 +6,15 @@ public:
         }
         flowerbed.insert(flowerbed.begin(), 0);
         flowerbed.push_back(0);
-        int i, j, k;
+        int i,j, k;
         i = 1;
-
         while(i < flowerbed.size()-1){
             j = i-1;
             k = i+1;
             if(n == 0){
                 break;
             }
-            if(flowerbed[j] == flowerbed[i] && flowerbed[i] == flowerbed[k]){
+            if(flowerbed[j]+flowerbed[i]+flowerbed[k] == 0){
                 flowerbed[i] = 1;
                 n--;
             }
