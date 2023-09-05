60085e35a796609d16a45483608826c8b8ba3399
Mariam Maghraby <61349498+Mariam-maghraby@users.noreply.github.com>
2023-09-05 16:26:29 +0300

    Time: 16 ms (55.95%), Space: 19.6 MB (31.84%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,15 +1,7 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        map<int, int>numsMap;
-
-        for(int i=0; i<nums.size(); i++){
-            numsMap[nums[i]]++;
-        }
-        auto pr = std::max_element(numsMap.begin(), numsMap.end(), [](const auto &x, const auto &y) {
-                    return x.second < y.second;
-                });
-        return pr->first;
-
+        sort(nums.begin(), nums.end());
+        return nums[ceil(nums.size()/2)];
     }
 };
