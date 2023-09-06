3924ae516a2483be38a0ea9c2ca00e90bb632b1a
Mariam Maghraby <61349498+Mariam-maghraby@users.noreply.github.com>
2023-09-06 18:20:09 +0300

    Time: 614 ms (8.42%), Space: 31.1 MB (86.22%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int sumLeft = 0;
+        int sumRight = 0;
+
+        for(int i=0; i<nums.size(); i++){
+            sumLeft = accumulate(nums.begin(), nums.begin()+i, 0);
+            sumRight = accumulate(nums.begin()+i+1, nums.end(), 0);
+            if(sumLeft == sumRight){
+                return i;
+            }
+        }
+    return -1;
+        
+    }
+};
