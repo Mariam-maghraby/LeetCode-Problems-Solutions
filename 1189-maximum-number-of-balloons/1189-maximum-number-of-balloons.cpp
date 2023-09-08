a27f59afd4fc55652c9d26d1f6dfa9a9c5a6eb67
Mariam Maghraby <61349498+Mariam-maghraby@users.noreply.github.com>
2023-09-08 23:52:23 +0300

    Time: 3 ms (66.13%), Space: 6.8 MB (14.60%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -1,33 +1,15 @@
 class Solution {
 public:
     int maxNumberOfBalloons(string text) {
-       vector<float> count(5);
+       map<char, int>mp;
         for(int i=0; i<text.size(); i++){
-            if(text[i] == 'b'){
-                count[0]++;
-            }
-            else{
-                if(text[i] == 'a')
-                count[1]++;
-
-                else {
-                    if (text[i] == 'l')
-                        count[2] = count[2] + 0.5;
-
-                    else {
-                        if (text[i] == 'o')
-                            count[3] = count[3] + 0.5;
-
-                        else {
-                            if (text[i] == 'n')
-                                count[4]++;
-
-                        }
-
-                    }
-                }
-            }
+            mp[text[i]]++;
         }
-        return int(*min_element(count.begin(), count.end()));
+        int b = mp['b'];
+        int a = mp['a'];
+        int l = mp['l']/2;
+        int o = mp['o']/2;
+        int n = mp['n'];
+        return min({b, a, l, o, n});
     }
 };
