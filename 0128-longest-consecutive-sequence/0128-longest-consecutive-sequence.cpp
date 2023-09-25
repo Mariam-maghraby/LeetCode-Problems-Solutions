c86183b880dae4568306f5a2169fb0a6dd105e4b
Mariam Maghraby <61349498+Mariam-maghraby@users.noreply.github.com>
2023-09-25 16:10:17 +0300

    Time: 59 ms (97.21%), Space: 43.8 MB (75.82%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -5,6 +5,7 @@ public:
             return 1;
         }
         sort(nums.begin(), nums.end());
+        
         if(nums.size() == 2){
             if(nums[1] == nums[0]+1){
                 return 2;
@@ -12,25 +13,21 @@ public:
             return 1;
         }
         unique(nums.begin(), nums.end());
-        vector<vector<int>>possibleSolutions;
-        vector<int> subSolution;
-
+        int max = 0;
+        int count = 0;
         for(int i=0; i<nums.size(); i++){
             if((i+1) < nums.size() && nums[i]+1 == nums[i+1]){
-                subSolution.push_back(nums[i]);
+                count++;
             }
             else{
-                subSolution.push_back(nums[i]);
-                possibleSolutions.push_back(subSolution);
-                subSolution.clear();
+                count++;
+                if(max < count){
+                    max = count;
+                }
+                count = 0;
             }
         }
-        int max = 0;
-        for(int i=0; i<possibleSolutions.size(); i++){
-            if(max<possibleSolutions[i].size()){
-                max = possibleSolutions[i].size();
-            }
-        }
+
         return max;
     }
 };
