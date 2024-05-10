064827797b878f0cb0f463bd0ed1b7328b7cb8f9
Mariam Maghraby <61349498+Mariam-maghraby@users.noreply.github.com>
2024-05-10 20:24:34 +0300

    Time: 50 ms (73.21%), Space: 48.7 MB (77.81%) - LeetHub

--- 2634-filter-elements-from-array/2634-filter-elements-from-array.js
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -5,8 +5,10 @@
  */
 var filter = function (arr, fn) {
     const newArr = [];
-    arr.forEach((element, index) => Boolean(fn(element, index)) === true ? newArr.push(element) : null)
-
+    for (let i = 0; i < arr.length; i++) {
+        if (Boolean(fn(arr[i], i)) === true)
+            newArr.push(arr[i])
+    }
     return newArr;
 
 };
