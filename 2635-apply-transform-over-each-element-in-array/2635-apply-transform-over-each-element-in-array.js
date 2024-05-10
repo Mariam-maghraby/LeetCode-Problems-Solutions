be859e251647f1c484bf74e2c779c13802e44f6b
Mariam Maghraby <61349498+Mariam-maghraby@users.noreply.github.com>
2024-05-10 20:13:01 +0300

    Time: 57 ms (22.43%), Space: 48.8 MB (58.14%) - LeetHub

--- 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -3,10 +3,9 @@
  * @param {Function} fn
  * @return {number[]}
  */
-var map = function(arr, fn) {
-    for(let i=0; i<arr.length; i++){
-        arr[i] = fn(arr[i], i);
-    }
-    return arr;
-    
+var map = function (arr, fn) {
+    const newArr = [];
+    arr.forEach((element, index) => newArr.push(fn(element, index)))
+    return newArr;
+
 };
