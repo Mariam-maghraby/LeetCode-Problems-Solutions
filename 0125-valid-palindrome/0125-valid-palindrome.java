238a392f34bc3dd16f72fadbcf64bf2e0389e122
Mariam Maghraby <61349498+Mariam-maghraby@users.noreply.github.com>
2024-09-21 14:27:56 +0300

    Time: 15 ms (25.04%), Space: 45.1 MB (19.53%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,19 +1,12 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        boolean res = false;
         String string = s.replaceAll("[^a-zA-Z0-9]+", "").toLowerCase();
         int len = string.length();
-        if(len == 0){
-            return true;
-        }
-        int maxPtr  = len/2;
-        for(int i=0; i<maxPtr; i++){
-
-            if(string.charAt(i) != string.charAt(len-i-1)){
-                return false;
-            }
-        }
-        res = true;
-        return res;
+        StringBuilder stringBuilder = new StringBuilder();
+       for(int i=len-1; i>=0; i--){
+        stringBuilder.append(string.charAt(i));
+       }
+        String reversed = stringBuilder.toString();
+        return reversed.equals(string);
     }
 }
