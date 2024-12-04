800c21428fc51e149b3537af161b4b8990b25e60
Mariam Maghraby <61349498+Mariam-maghraby@users.noreply.github.com>
2024-12-04 22:42:53 +0200

    Time: 1 ms (81.30%), Space: 26.7 MB (13.13%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
-bool isPalindrome(string s) {
-    int l = 0;
-    int r = s.size()-1;
+bool isPalindrome(string s, int l, int r) {
     while(l<=r){ 
         if(s[l] != s[r]){
             return false;
@@ -21,9 +19,7 @@ bool isPalindrome(string s) {
                 r--;
             }
             else{
-                string tmp1 = s;
-                string tmp2 = s;
-                return isPalindrome(tmp1.erase(l, 1))|| isPalindrome(tmp2.erase(r, 1));
+                return isPalindrome(s, ++l, r)|| isPalindrome(s, --l, --r);
 
             }
         }
