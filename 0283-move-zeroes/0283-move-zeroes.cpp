c2e0ebb75d5c30506525b15a2cc0b7ccdc24223b
Mariam Maghraby <61349498+Mariam-maghraby@users.noreply.github.com>
2024-12-13 17:44:46 +0200

    Time: 0 ms (100.00%), Space: 24.5 MB (12.23%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -2,21 +2,15 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int l=0;
-        int s=0;
         vector<int> nums2(nums.size());
         if(nums.size() != 1){
             for(int i=0; i<nums.size(); i++){
                 if(nums[i]!=0){
-                    nums[l] = nums[i];
-                    l++;
-                    
-                }else{
-                    s++;
+                    nums[l++] = nums[i];
                 }
             }
-           while(s){
-                nums[nums.size()-s] = 0;
-                s--;
+           while(l<nums.size()){
+                nums[l++] = 0;
             }
         }
     }
