3fbda8a2b55404c8265273da844a41f91966a11c
Mariam Maghraby <61349498+Mariam-maghraby@users.noreply.github.com>
2024-12-21 22:58:33 +0200

    Time: 23 ms (8.95%), Space: 22.9 MB (34.28%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        if(piles.size() >= h){
+            return *max_element(piles.begin(), piles.end());
+        }
+        int l=1;
+        long long r=1e9+1;
+        while(l<r){
+            int sum=0;
+            int m = (r+l)/2;
+            for(int i=0; i<piles.size(); i++){
+                sum+=ceil(piles[i]/(double)m);
+            }
+                if(sum>h){
+                    l=m+1;
+                }else{
+                    r=m;
+
+                }
+        }
+        return r;
+
+    }
+};
