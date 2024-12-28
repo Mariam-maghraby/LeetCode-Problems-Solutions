06e2ee8898588316a8b78404fbaca1bf968d1a26
Mariam Maghraby <61349498+Mariam-maghraby@users.noreply.github.com>
2024-12-28 17:22:19 +0200

    Time: 910 ms, Memory: 0B - LeetHub

--- 0814-87-1633-percentage-of-users-attended-a-contest/0814-87-1633-percentage-of-users-attended-a-contest.sql
+++ 0814-87-1633-percentage-of-users-attended-a-contest/0814-87-1633-percentage-of-users-attended-a-contest.sql
@@ -1,6 +1,6 @@
 SELECT 
     contest_id, 
-    ROUND(COUNT(*)*100/ (select count(*) from Users), 2)as percentage 
+    ROUND(COUNT(DISTINCT r.user_id)*100/ (select count(*) from Users), 2)as percentage 
 from Register as r
 JOIN Users as u
     ON u.user_id = r.user_id
