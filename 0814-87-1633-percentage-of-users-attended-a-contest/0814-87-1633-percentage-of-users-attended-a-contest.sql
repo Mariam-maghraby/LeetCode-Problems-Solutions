8f7161303b72ba70f15dfa28170bcc82bcc88b11
Mariam Maghraby <61349498+Mariam-maghraby@users.noreply.github.com>
2024-12-28 17:17:14 +0200

    Time: 1889 ms, Memory: 0B - LeetHub

---
+++ 0814-87-1633-percentage-of-users-attended-a-contest/0814-87-1633-percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,8 @@
+SELECT 
+    contest_id, 
+    ROUND(COUNT(*)*100/ (select count(*) from Users), 2)as percentage 
+from Register as r
+JOIN Users as u
+    ON u.user_id = r.user_id
+GROUP BY r.contest_id
+ORDER BY percentage DESC, r.contest_id 
