9764f6e27d913a8d6b2701c81c9b06e32faae3e1
Mariam Maghraby <61349498+Mariam-maghraby@users.noreply.github.com>
2024-12-28 18:28:13 +0200

    Time: 639 ms, Memory: 0B - LeetHub

--- 0753-474-1211-queries-quality-and-percentage/0753-474-1211-queries-quality-and-percentage.sql
+++ 0753-474-1211-queries-quality-and-percentage/0753-474-1211-queries-quality-and-percentage.sql
@@ -1,4 +1,4 @@
-SELECT 
+ SELECT 
     query_name, 
     ROUND(AVG(rating/position) , 2) as quality,
     ROUND(COUNT(case when rating < 3 then 1 else null end)*100 /COUNT(*), 2) as poor_query_percentage
