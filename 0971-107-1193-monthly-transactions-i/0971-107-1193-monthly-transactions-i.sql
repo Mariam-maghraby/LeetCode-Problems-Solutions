a1ac4fa85fc106af85fa4539dd2aedddce72c7b1
Mariam Maghraby <61349498+Mariam-maghraby@users.noreply.github.com>
2025-01-04 14:15:00 +0200

    Time: 980 ms, Memory: 0B - LeetHub

--- 0971-107-1193-monthly-transactions-i/0971-107-1193-monthly-transactions-i.sql
+++ 0971-107-1193-monthly-transactions-i/0971-107-1193-monthly-transactions-i.sql
@@ -5,5 +5,5 @@ SELECT
     COUNT(case when state = "approved" then 1 ELSE NULL END) as approved_count,
     SUM(amount) as trans_total_amount,
     SUM(case when state = "approved" then amount ELSE 0 END) as approved_total_amount
-from Transactions
+FROM Transactions
 GROUP BY country, EXTRACT(MONTH FROM trans_date), EXTRACT(YEAR FROM trans_date)
