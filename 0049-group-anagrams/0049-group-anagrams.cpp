4249964c4c6ebbf0e7b1d77a95e242c27af72afa
Mariam Maghraby <61349498+Mariam-maghraby@users.noreply.github.com>
2025-07-13 22:40:43 +0300

    Time: 18 ms (53.7%), Space: 24.8 MB (84.93%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<vector<string>> solution;
-        map<string, vector<string>>mp;
+        map <string, vector<string>>mp;
+        vector<vector<string>> ans;
         for(int i=0; i<strs.size(); i++){
-            string sorted = strs[i];
-            sort(sorted.begin(), sorted.end());
-                mp[sorted].push_back(strs[i]);
+            string or_str = strs[i];
+            sort(or_str.begin(), or_str.end());
+            if(mp.find(or_str) != mp.end()){
+                mp.at(or_str).push_back(strs[i]);
+            }else{
+                mp[or_str] = {strs[i]};
+            }
         }
-        for (auto i = mp.begin(); i != mp.end(); i++){
-            solution.push_back(i->second);
+
+        for(auto it = mp.begin(); it != mp.end(); it++){
+            ans.push_back(it->second);
         }
-        return solution;  
+        return ans;
     }
 };
