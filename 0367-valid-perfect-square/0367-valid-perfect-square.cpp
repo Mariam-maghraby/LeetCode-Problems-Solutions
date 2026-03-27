5b1083c5dd277eca033d8dcf16a6b342ea2e6fb4
Mariam Maghraby <61349498+Mariam-maghraby@users.noreply.github.com>
2026-03-28 01:25:25 +0200

    Time: 0 ms (100%), Space: 7.9 MB (17.74%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.cpp
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
-    set<long>mp;
         for(int i=0; i<46341; i++){
-            mp.insert(i*i);
+            if(i*i == num){
+                return true; 
+            }
         }
-        auto it = find(mp.begin(), mp.end(), num);
-        if(it != mp.end()) return true;
         return false;
     }
 };
