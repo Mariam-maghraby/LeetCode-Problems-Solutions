d231bebbdaecbd2f824eceeaae252803a29cf5a5
Mariam Maghraby <61349498+Mariam-maghraby@users.noreply.github.com>
2026-06-08 16:27:40 +0300

    Time: 6 ms (13.52%), Space: 8.6 MB (95.69%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        int i=1;
+        while(n != 0){
+            n=n-i;
+            i++;
+            if(n<=0){
+                break;
+            }
+        }
+       return n==0? i-1: i-2;
+    }
+};
