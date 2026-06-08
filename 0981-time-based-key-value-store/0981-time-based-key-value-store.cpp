1bdf5299b15334a0b33ae4b89ab17077fe4ee32e
Mariam Maghraby <61349498+Mariam-maghraby@users.noreply.github.com>
2026-06-09 00:12:30 +0300

    Time: 41 ms (80.83%), Space: 136.9 MB (59.74%) - LeetHub - Time: O(?), Space: O(?)

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.cpp
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.cpp
@@ -2,53 +2,33 @@ class TimeMap {
 public:
     TimeMap() {
     }
-    map<string,map<int,string>> mp;
-    
+    map<string,vector<pair<int,string>>> mp;
     
     void set(string key, string value, int timestamp) {
-        mp[key][timestamp]= value;
+        mp[key].push_back({timestamp,value});
     }
     
     string get(string key, int timestamp) {
         if (mp.find(key) == mp.end()) {
             return "";
         }
-        map<int, string>& inner_map = mp.at(key);
-        
-        auto it = inner_map.upper_bound(timestamp);
-        
-        // If the iterator points to the very beginning, it means 
-        // ALL stored timestamps are larger than the one we are looking for.
-        if (it == inner_map.begin()) {
-            return "";
+        vector<pair<int,string>>& inner_map = mp.at(key);
+     
+        int l = 0;
+        int r = inner_map.size()-1;
+        string res;
+        while(l<=r){
+            int mid =(l+r)/2;
+            if(inner_map[mid].first <= timestamp){
+                res = inner_map[mid].second;
+                l= mid+1;
+            }else{
+                
+                r  = mid-1;
+            }
         }
         
-        // 3. Move the iterator back by 1 step.
-        // This gives us the largest timestamp that is less than or equal to the target.
-        it--;
-        
-        return it->second;
-
-        // if (inner_map.find(timestamp) != inner_map.end()) {
-        //     return inner_map[timestamp];
-        // }
-        
-        // int l = 0;
-        // int r = inner_map.size();
-        // while(l<r){
-        //     int mid =(l+r)/2;
-        //     if(mid == timestamp){
-        //         return inner_map[timestamp];
-        //     }else{
-        //         if(mid>timestamp){
-        //             r  = mid-1;
-        //         }else{
-        //             l = mid+1;
-        //         }
-        //     }
-        // }
-        
-        // return "";
+        return res;
     }
 };
 
