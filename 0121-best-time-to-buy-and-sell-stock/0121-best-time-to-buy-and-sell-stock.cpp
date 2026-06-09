09bff9981b72c6e0579333ee5d8bb55482db1c73
Mariam Maghraby <61349498+Mariam-maghraby@users.noreply.github.com>
2026-06-09 03:24:46 +0300

    Time: 4 ms (18.27%), Space: 97.4 MB (59.72%) - LeetHub - Time: O(n), Space: O

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int i=0;
+        int j=1;
+        int minPrice = prices[i];
+        int maxPrice =  0;
+        int maxProfit =0;
+        int buyIndx=i;
+        int sellIndx=j;
+        while(j<prices.size()){
+            if(minPrice> prices[j] && j<prices.size()-1){
+                minPrice = prices[j];
+                buyIndx=j;
+            }
+
+            if(maxPrice< prices[j]){
+                maxPrice = prices[j];
+                sellIndx=j;
+            }
+            if(buyIndx < j){
+                maxProfit = max(maxProfit, prices[j]-minPrice);
+            }
+
+            j++;
+        }
+
+        return  maxProfit;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
