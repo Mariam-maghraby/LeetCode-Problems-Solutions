66d96291347b6d04db662f1dba2953a0baaf1f3e
Mariam Maghraby <61349498+Mariam-maghraby@users.noreply.github.com>
2026-06-16 11:35:08 +0300

    Time: 0 ms (100%), Space: 17 MB (65.29%) - LeetHub - Time: O(n), Space: O(n)

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -5,45 +5,37 @@ public:
         stack<int>sk;
         sk.push(stoi(tokens[0]));
         for(int i=1;i<tokens.size();i++){
-            if(tokens[i] == "+"){
+            if(tokens[i] == "+" || tokens[i] == "-" || tokens[i] == "*" || tokens[i] == "/"){
                 int x = sk.top();
                 sk.pop();
                 int y = sk.top();
                 sk.pop();
-                op=(x+y);
-                sk.push(op);
-            }else{
-                if(tokens[i] == "-"){
-                    int x = sk.top();
-                    sk.pop();
-                    int y = sk.top();
-                    sk.pop();
-                    op=(y-x);
+                if(tokens[i] == "+"){
+                    op=(x+y);
                     sk.push(op);
-                
                 }else{
-                    if(tokens[i] == "*"){
-                        int x = sk.top();
-                        sk.pop();
-                        int y = sk.top();
-                        sk.pop();
-                        op=(x*y);
+                    if(tokens[i] == "-"){
+                        
+                        op=(y-x);
                         sk.push(op);
-                
+                    
                     }else{
-                        if(tokens[i] == "/"){
-                            int x = sk.top();
-                            sk.pop();
-                            int y = sk.top();
-                            sk.pop();
-                            op=(y/x);
+                        if(tokens[i] == "*"){
+                    
+                            op=(x*y);
                             sk.push(op);
-                
+                    
                         }else{
-                            sk.push(stoi(tokens[i]));
+                            if(tokens[i] == "/"){
+                                op=(y/x);
+                                sk.push(op);
+                    
+                            }
                         }
                     }
                 }
+            }else{
+                sk.push(stoi(tokens[i]));
             }
         }
         return op;
