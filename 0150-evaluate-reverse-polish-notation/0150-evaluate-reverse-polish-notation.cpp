51570f9867964505d06207d6f9d267adf82a75cf
Mariam Maghraby <61349498+Mariam-maghraby@users.noreply.github.com>
2026-06-16 11:08:56 +0300

    Time: 0 ms (100%), Space: 17.2 MB (40.37%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        int op=stoi(tokens[0]);
+        stack<int>sk;
+        sk.push(stoi(tokens[0]));
+        for(int i=1;i<tokens.size();i++){
+            if(tokens[i] == "+"){
+                int x = sk.top();
+                sk.pop();
+                int y = sk.top();
+                sk.pop();
+                op=(x+y);
+                sk.push(op);
+            }else{
+                if(tokens[i] == "-"){
+                    int x = sk.top();
+                    sk.pop();
+                    int y = sk.top();
+                    sk.pop();
+                    op=(y-x);
+                    sk.push(op);
+                
+                }else{
+                    if(tokens[i] == "*"){
+                        int x = sk.top();
+                        sk.pop();
+                        int y = sk.top();
+                        sk.pop();
+                        op=(x*y);
+                        sk.push(op);
+                
+                    }else{
+                        if(tokens[i] == "/"){
+                            int x = sk.top();
+                            sk.pop();
+                            int y = sk.top();
+                            sk.pop();
+                            op=(y/x);
+                            sk.push(op);
+                
+                        }else{
+                            sk.push(stoi(tokens[i]));
+                        }
+                    }
+                }
+            }
+        }
+        return op;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
