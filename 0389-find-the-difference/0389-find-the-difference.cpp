9f1e0527546fda50a3a104b698d9e48933222745
Mariam Maghraby <61349498+Mariam-maghraby@users.noreply.github.com>
2026-06-17 14:55:32 +0300

    Time: 2 ms (16.61%), Space: 9.2 MB (76.65%) - LeetHub - Time: O(n log n), Space: O(1)

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        sort(t.begin(),t.end());
+        sort(s.begin(),s.end());
+
+        int  i=0;
+        int  j=0;
+
+        while(j<t.size()){
+            if(s[i]== t[j]){
+                i++;
+                j++;
+            }else{
+                break;
+            }
+
+        }
+        return t[j];
+    }
+
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
