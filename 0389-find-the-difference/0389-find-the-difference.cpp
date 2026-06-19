daabddf35700408a01b6e541986c5ba6325d3613
Mariam Maghraby <61349498+Mariam-maghraby@users.noreply.github.com>
2026-06-19 15:13:43 +0300

    Time: 0 ms (100%), Space: 9.2 MB (49.97%) - LeetHub - Time: O(n + m), Space: O(1)

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,14 +1,8 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        int sumS =0;
-        int sumT=0;
-        for(int i=0;i<t.size();i++){
-            sumT+=static_cast<int>(t[i]);
-        }
-        for(int i=0;i<s.size();i++){
-            sumS+=static_cast<int>(s[i]);
-        }
+        int sumT = std::accumulate(t.begin(), t.end(), 0);
+        int sumS = std::accumulate(s.begin(), s.end(), 0);
         return static_cast<char>(sumT-sumS);
     }
 };
