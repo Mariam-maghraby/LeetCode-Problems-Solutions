20d60fa16a467076cfafbea4d288b3298459af67
Mariam Maghraby <61349498+Mariam-maghraby@users.noreply.github.com>
2026-06-19 15:06:17 +0300

    Time: 5 ms (1.48%), Space: 9.2 MB (49.97%) - LeetHub - Time: O(n + m), Space: O(1)

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        map<char, int>mp;
-        map<char, int>mp2;
+        int sumS =0;
+        int sumT=0;
         for(int i=0;i<t.size();i++){
-            mp[t[i]]+=1;
+            sumT+=static_cast<int>(t[i]);
         }
-        for(int j=0;j<s.size();j++){
-            mp2[s[j]]+=1;
+        for(int i=0;i<s.size();i++){
+            sumS+=static_cast<int>(s[i]);
         }
-        for (auto it = mp.begin(); it != mp.end(); ++it) {
-            char character = it->first;
-            int countInT = it->second;
-
-            // If the character is missing from 's', or its count doesn't match up
-            if (mp2.find(character) == mp2.end() || mp2[character] != countInT) {
-                return character;
-            }
-        }
-        return mp.end()->first;
+        return static_cast<char>(sumT-sumS);
     }
 };
 
