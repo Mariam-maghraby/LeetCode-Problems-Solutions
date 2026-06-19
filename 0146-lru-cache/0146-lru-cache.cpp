a93683cddcab66fb3a06b998f42fcb91cf58aaec
Mariam Maghraby <61349498+Mariam-maghraby@users.noreply.github.com>
2026-06-20 00:37:04 +0300

    Time: 100 ms (31.17%), Space: 190.1 MB (5.05%) - LeetHub - Time: O(1), Space: O(capacity)

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -5,10 +5,9 @@ public:
     int capacity;
     unordered_map<int,pair<int,list<int>::iterator>>mp;
     LRUCache(int capacity) {
-        this->mp.reserve(capacity);
         this->capacity = capacity;
-
     }
+    
     void arrangeLRU(int key) {
         //arrange mruKeys & update lruKey
         // if key exists remove
@@ -32,23 +31,24 @@ public:
         return -1;
     }
     void put(int key, int value) {
-            if(get(key) != -1){
-                mp[key]={value, mruKeys.begin()};
+        if(get(key) != -1){
+            mp[key]={value, mruKeys.begin()};
+        }else{
+            mruKeys.push_front(key);
+            if(mp.size()<capacity){
+                mp.insert({key, {value, mruKeys.begin()}});
             }else{
-                mruKeys.push_front(key);
-                if(mp.size()<capacity){
-                    mp.insert({key, {value, mruKeys.begin()}});
-                }else{
-                    //remove LRU element
-                    //then add the new key with its value 
-                    removeLRU();
-                    mp.insert({key, {value, mruKeys.begin()}});
-                }
+                //remove LRU element
+                //then add the new key with its value 
+                removeLRU();
+                mp.insert({key, {value, mruKeys.begin()}});
             }
-            arrangeLRU(key);
         }
+        arrangeLRU(key);
+    }
 
 };
+
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
