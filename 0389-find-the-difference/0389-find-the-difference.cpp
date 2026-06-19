72ee43aaf5f8cdcd4189f4d88f1a6dedbef4cc35
Mariam Maghraby <61349498+Mariam-maghraby@users.noreply.github.com>
2026-06-19 14:58:40 +0300

    Time: 3 ms (12.13%), Space: 9.4 MB (25.36%) - LeetHub - Time: O(n + m), Space: O(n + m)

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        sort(t.begin(),t.end());
-        sort(s.begin(),s.end());
-
-        int  i=0;
-        int  j=0;
-
-        while(i<s.size()){
-            if(s[i]== t[j]){
-                i++;
-                j++;
-            }else{
-                break;
-            }
-
+        map<char, int>mp;
+        map<char, int>mp2;
+        for(int i=0;i<t.size();i++){
+            mp[t[i]]+=1;
         }
-        return t[i];
-    }
+        for(int j=0;j<s.size();j++){
+            mp2[s[j]]+=1;
+        }
+        for (auto it = mp.begin(); it != mp.end(); ++it) {
+            char character = it->first;
+            int countInT = it->second;
 
+            // If the character is missing from 's', or its count doesn't match up
+            if (mp2.find(character) == mp2.end() || mp2[character] != countInT) {
+                return character;
+            }
+        }
+        return mp.end()->first;
+    }
 };
 
 // Synced seamlessly with LeetHub Pro
